737d521337e0051bcdf46161dc0f97fd6bd341b9
jdidion <github@didion.net>
2023-02-01 10:58:22 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pest-wdl
-Pest parser grammar for Workflow Description Language (WDL)
+# pest-wdl-1
+
+[Pest](https://pest.rs/) parser grammar for Workflow Description Language (WDL).
+
+## Usage
+
+```wdl
+use pest::iterators::Pair;
+use pest_wdl_1 as wdl;
+
+fn main() {
+    let text = r#"
+    version 1.0
+
+    workflow foo {
+    }
+    "#;
+    let root: Pair<'_, wdl::Rule> = wdl::parse_document(text).expect("Error parsing WDL document");
+    ...
+}
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use pest::{
+    error::{Error, ErrorVariant},
+    iterators::Pair,
+    Parser, Position,
+};
+use pest_derive;
+
+#[derive(pest_derive::Parser)]
+#[grammar = "wdl.pest"]
+pub struct PestParser;
+
+pub fn parse_document<'a, Text: AsRef<str>>(text: &'a str) -> Result<Pair<'a, Rule>, Error<Rule>> {
+    let mut root_pairs = PestParser::parse(Rule::document, text)?;
+    root_pairs.next().ok_or_else(|| {
+        Error::new_from_pos(
+            ErrorVariant::CustomError {
+                message: String::from("Empty WDL document"),
+            },
+            Position::from_start(text),
+        )
+    })
+}
