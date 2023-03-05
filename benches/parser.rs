fd9e375cdce4514f395decde9cd0e59e7b954ae7
jdidion <github@didion.net>
2023-03-05 06:20:02 -0800

    add benches

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use pest_wdl_1;
+use pest::iterators::Pair;
+use pest_wdl_1::Rule;
 use std::{fs, path::PathBuf};
 
 pub fn benchmark_parser(c: &mut Criterion) {
@@ -7,16 +8,16 @@ pub fn benchmark_parser(c: &mut Criterion) {
     let test_dir = root.join("resources").join("test");
     let mut group = c.benchmark_group("parser");
 
-    let comprehensive_path = test_dir.join("strings.wdl");
-    let comprehensive_text = fs::read_to_string(comprehensive_path.clone()).unwrap();
+    let strings_path = test_dir.join("strings.wdl");
+    let strings_text = fs::read_to_string(strings_path.clone()).unwrap();
     group.bench_function("strings", |b| {
-        b.iter(|| pest_wdl_1::parse_document(&comprehensive_text))
+        b.iter(|| pest_wdl_1::parse_document(&strings_text))
     });
 
-    let comprehensive_path = test_dir.join("expressions.wdl");
-    let comprehensive_text = fs::read_to_string(comprehensive_path.clone()).unwrap();
+    let expressions_path = test_dir.join("expressions.wdl");
+    let expressions_text = fs::read_to_string(expressions_path.clone()).unwrap();
     group.bench_function("expressions", |b| {
-        b.iter(|| pest_wdl_1::parse_document(&comprehensive_text))
+        b.iter(|| pest_wdl_1::parse_document(&expressions_text))
     });
 
     let comprehensive_path = test_dir.join("comprehensive.wdl");
@@ -26,5 +27,24 @@ pub fn benchmark_parser(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_parser);
+fn exhaust(pair: Pair<Rule>) {
+    for child in pair.into_inner() {
+        exhaust(child)
+    }
+}
+
+pub fn benchmark_parser_exhaust(c: &mut Criterion) {
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let test_dir = root.join("resources").join("test");
+    let comprehensive_path = test_dir.join("comprehensive.wdl");
+    let comprehensive_text = fs::read_to_string(comprehensive_path.clone()).unwrap();
+    c.bench_function("parser_exhaust", |b| {
+        b.iter(|| {
+            let doc = pest_wdl_1::parse_document(&comprehensive_text).unwrap();
+            exhaust(doc)
+        })
+    });
+}
+
+criterion_group!(benches, benchmark_parser, benchmark_parser_exhaust);
 criterion_main!(benches);
